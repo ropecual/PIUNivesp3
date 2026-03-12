9225dfcb0d73b73cd3640da9a1cc53fc33103fbf
Roni Ferraresso <rope.alvarenga@gmail.com>
2026-03-12 12:38:45 -0300

    feat: Implement a comprehensive landing page with new layout, dedicated sections, images, and updated dependencies.

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,12 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+	"staticfiles": {
+		"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+	},
+}
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
